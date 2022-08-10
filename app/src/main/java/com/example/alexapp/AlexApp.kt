e3093c152f002716bc85778b18b485ae76ce23ac
TurtlePU <sokolov.p64@gmail.com>
2022-08-10 23:21:35 +0300

    + Rate component

--- app/src/main/java/com/example/alexapp/AlexApp.kt
+++ app/src/main/java/com/example/alexapp/AlexApp.kt
@@ -1,5 +1,6 @@
 package com.example.alexapp
 
+import Performance
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -8,10 +9,14 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.alexapp.Credentials.Companion.credentials
 import com.example.alexapp.ui.theme.AlexAppTheme
 
 @Composable
-fun AlexApp(checkCredentials: suspend (Credentials) -> String?) {
+fun AlexApp(
+  checkCredentials: suspend (Credentials) -> String?,
+  performances: (Credentials) -> List<Performance>
+) {
   AlexAppTheme {
     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
       val navController = rememberNavController()
@@ -19,7 +24,13 @@ fun AlexApp(checkCredentials: suspend (Credentials) -> String?) {
         composable("auth") {
           Auth(checkCredentials) { navController.navigate("rate/$it") }
         }
+        composable("rate/{host}:{login}:{token}") {
+          val credentials = it.arguments?.credentials ?: throw MissingArguments()
+          Rate(performances(credentials))
+        }
       }
     }
   }
 }
+
+class MissingArguments : Throwable()
---
+++ app/src/main/java/com/example/alexapp/Rate.kt
@@ -0,0 +1,8 @@
+package com.example.alexapp
+
+import Performance
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Rate(performances: List<Performance>) {
+}
