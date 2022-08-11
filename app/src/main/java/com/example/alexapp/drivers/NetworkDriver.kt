02d567fd8e0dbfc073faef34baf4e954c47538da
TurtlePU <sokolov.p64@gmail.com>
2022-08-11 16:55:56 +0300

    + basic network requests

--- app/src/main/java/com/example/alexapp/drivers/NetworkDriver.kt
+++ app/src/main/java/com/example/alexapp/drivers/NetworkDriver.kt
@@ -1,14 +1,50 @@
 package com.example.alexapp.drivers
 
 import Performance
+import PostAuth
+import PostGrade
+import Protocol
+import android.util.Log
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.example.alexapp.models.AuthorizationModel.Credentials
 import com.example.alexapp.models.RestoreModel.Rating
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.features.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
 
 object NetworkDriver : AppDriver {
+  private val client = HttpClient(CIO) {
+    install(JsonFeature) {
+      serializer = KotlinxSerializer(Protocol.json)
+    }
+    install(Logging) {
+      logger = object : Logger {
+        override fun log(message: String) {
+          Log.i("Network", message)
+        }
+      }
+      level = LogLevel.ALL
+    }
+    install(HttpTimeout) {
+      socketTimeoutMillis = 3_000
+      requestTimeoutMillis = 3_000
+      connectTimeoutMillis = 3_000
+    }
+    defaultRequest {
+      contentType(ContentType.Application.Json)
+      accept(ContentType.Application.Json)
+    }
+  }
+
   override fun flow(host: String) = Pager(PagingConfig(100)) {
     object : PagingSource<Int, Performance>() {
       override fun getRefreshKey(state: PagingState<Int, Performance>): Int? {
@@ -22,10 +58,22 @@ object NetworkDriver : AppDriver {
   }.flow
 
   override suspend fun rate(credentials: Credentials, performance: Performance, rating: Rating) {
-    TODO("Not yet implemented")
+    val (host, login, token) = credentials
+    val (grade, comment) = rating
+    val response: HttpResponse = client.post("$host/grade") {
+      body = PostGrade(login, token, performance, grade, comment)
+    }
+    assert(response.status == HttpStatusCode.OK)
   }
 
-  override suspend fun authorize(credentials: Credentials): String? {
-    TODO("Not yet implemented")
+  override suspend fun authorize(credentials: Credentials) = try {
+    val (host, login, token) = credentials
+    val response: HttpResponse = client.post("$host/auth") {
+      body = PostAuth(login, token)
+    }
+    assert(response.status == HttpStatusCode.OK || response.status == HttpStatusCode.Created)
+    null
+  } catch (e: Throwable) {
+    e.localizedMessage
   }
 }
