2401f7a2d76630a7f188c14119ff71af52dc2fdc
TurtlePU <sokolov.p64@gmail.com>
2022-09-07 16:06:58 +0300

    split up preferences object

--- app/src/main/java/com/example/alexapp/MainActivity.kt
+++ app/src/main/java/com/example/alexapp/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.datastore.preferences.preferencesDataStore
+import com.example.alexapp.authorization.AuthorizationPreferences
 import com.example.alexapp.authorization.authorize
 import com.example.alexapp.performance.NetworkRatings
+import com.example.alexapp.performance.RatingPreferences
 
 class MainActivity : ComponentActivity() {
   companion object {
@@ -15,11 +17,10 @@ class MainActivity : ComponentActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    val model = PreferencesModel(dataStore)
     setContent {
       AppLayout(
-        model,
-        model,
+        AuthorizationPreferences(dataStore),
+        RatingPreferences(dataStore),
         { authorize(ktorClient, it) },
         { NetworkRatings(ktorClient, it) })
     }
