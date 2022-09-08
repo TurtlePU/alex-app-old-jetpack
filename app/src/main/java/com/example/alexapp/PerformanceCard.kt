afb1b6f27e7e30113615a47bfbb0a4bcd174a14b
TurtlePU <sokolov.p64@gmail.com>
2022-09-08 10:32:51 +0300

    simple performance card

--- app/src/main/java/com/example/alexapp/PerformanceCard.kt
+++ app/src/main/java/com/example/alexapp/PerformanceCard.kt
@@ -2,11 +2,31 @@ package com.example.alexapp
 
 import Participant
 import Performance
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun PerformanceCard(performance: Performance, isNew: Boolean = false, onClick: () -> Unit = {}) {
+  Column(
+    modifier = Modifier
+      .padding(15.dp)
+      .fillMaxWidth()
+      .selectable(selected = false, onClick = onClick)
+      .apply { if (isNew) background(color = MaterialTheme.colorScheme.secondary) }
+  ) {
+    Text(text = performance.participantName, fontSize = 20.sp)
+    Text(text = performance.repertoire, fontSize = 20.sp)
+  }
 }
 
 @Preview
