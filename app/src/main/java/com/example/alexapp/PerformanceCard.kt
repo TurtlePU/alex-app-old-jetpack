e6ed7b80fcb48acbd6cf77fbaa01f68bfa521c81
TurtlePU <sokolov.p64@gmail.com>
2022-09-12 11:04:38 +0300

    fix performance card background

--- app/src/main/java/com/example/alexapp/PerformanceCard.kt
+++ app/src/main/java/com/example/alexapp/PerformanceCard.kt
@@ -2,30 +2,34 @@ package com.example.alexapp
 
 import Participant
 import Performance
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.alexapp.ui.theme.AlexAppTheme
 
 @Composable
 fun PerformanceCard(performance: Performance, isNew: Boolean = false, onClick: () -> Unit = {}) {
-  Column(
+  Surface(
+    color =
+    if (isNew) MaterialTheme.colorScheme.primary
+    else MaterialTheme.colorScheme.background,
     modifier = Modifier
-      .padding(15.dp)
       .fillMaxWidth()
       .selectable(selected = false, onClick = onClick)
-      .apply { if (isNew) background(color = MaterialTheme.colorScheme.secondary) }
   ) {
-    Text(text = performance.participantName, fontSize = 20.sp)
-    Text(text = performance.repertoire, fontSize = 20.sp)
+    Column(modifier = Modifier.padding(15.dp)) {
+      Text(text = performance.participantName, fontSize = 20.sp)
+      Text(text = performance.repertoire, fontSize = 20.sp)
+    }
   }
 }
 
@@ -35,5 +39,7 @@ fun PerformanceCardPreview() {
   val whiteStripes = Participant("the White Stripes", "alternative rock", "25")
   val sevenNationArmy = Performance(0, whiteStripes, "Seven Nation Army")
   var isNew by remember { mutableStateOf(true) }
-  PerformanceCard(sevenNationArmy, isNew) { isNew = !isNew }
+  AlexAppTheme {
+    PerformanceCard(sevenNationArmy, isNew) { isNew = !isNew }
+  }
 }
